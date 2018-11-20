f74b8b72c7b3bab520709a8babe95024c287b90f
voxelvoxelvoxel <andrew_ang@live.com>
2018-11-19 20:18:43 -0800

    Initial Project Setup

---
+++ Direct3D12UWPSandbox/Direct3D12UWPSandbox/pch.h
@@ -0,0 +1,62 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+#include <wrl.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include "d3dx12.h"
+
+#include <algorithm>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DescriptorHeap.h"
+#include "DirectXHelpers.h"
+#include "EffectPipelineStateDescription.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "PrimitiveBatch.h"
+#include "RenderTargetState.h"
+#include "ResourceUploadBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
+
+namespace DX
+{
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            // Set a breakpoint on this line to catch DirectX API errors
+            throw std::exception();
+        }
+    }
+}
